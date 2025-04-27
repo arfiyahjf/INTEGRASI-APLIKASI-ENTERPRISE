fe442a2a3e7b7efbc95d3d09ffc6d99ad443fb49
arfiyahjf <148057667+arfiyahjf@users.noreply.github.com>
2025-04-27 20:41:39 +0700

    Delete UTS-IAE_ms-profile-master/UTS-IAE_ms-profile-master directory

--- UTS-IAE_ms-profile-master/UTS-IAE_ms-profile-master/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-
-
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/register', [UserController::class, 'register']);
-
