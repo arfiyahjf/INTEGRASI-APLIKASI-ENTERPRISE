27df3f3b1785b8a290b693b36b03e3d6c413d66b
arfiyahjf <148057667+arfiyahjf@users.noreply.github.com>
2025-04-27 20:41:14 +0700

    Delete UTS-IAE_ms-loan-master directory

--- UTS-IAE_ms-loan-master/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoanController;
-
-Route::post('/loan/create', [LoanController::class, 'create']);
-Route::post('/loans/return/{id}', [LoanController::class, 'returnBook']);
